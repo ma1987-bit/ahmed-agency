65ab1d279cce20d390b000dc22c3b3ff35a7a46c
Ahmed Mohamed Omer Mahmoud (s123157) <mahmoud.ahmedmohamedomer@student.ap.be>
2022-10-28 22:54:27 +0200

    Gatsby pulgins finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 
+import { StaticImage } from 'gatsby-plugin-image'
 import * as React from 'react'
 import Layout from '../components/layout'
 
@@ -7,6 +8,10 @@ const IndexPage = () => {
     <main>
       <Layout pageTitle="Welcome to Ahmed Agency!">
       <p>Lorem ipsum</p>
+      <StaticImage
+      alt='foto from pexel website'
+      src='https://images.pexels.com/photos/13918700/pexels-photo-13918700.jpeg?auto=compress&cs=tinysrgb&w=200&lazy=load'
+      /> 
       </Layout>
       
     </main>
