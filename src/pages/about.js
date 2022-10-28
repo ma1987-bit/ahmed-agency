254e119ac32aa9f39ef6a8fc0441ba43d888964b
Ahmed Mohamed Omer Mahmoud (s123157) <mahmoud.ahmedmohamedomer@student.ap.be>
2022-10-28 22:25:14 +0200

    Finished React in Gatsby

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <Layout pageTitle="About Us">
+      <p>Artist Agency was founded in 1977 by founder, John Doe. AA continues to be at the forefront of art by establishing the careers of our talents on a holistic level -- and setting trends within the industry. </p>
+    </Layout>
+    </main>
+  )
+}
+
+
+export default AboutPage
